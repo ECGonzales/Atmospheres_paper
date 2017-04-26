18780ba57163cb89900958e23459bde9d53b6474
ECGonzales <gonza338@gmail.com>
2017-04-26 12:32:57 -0400

    pep-8 Changes

--- compplots.py
+++ compplots.py
@@ -2,31 +2,32 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# making a chnage to test new remotes!
+
 # ------------------------------------------------------------------------------------
 # ------------------- Read in Spectra and Photometry files ---------------------------
 # -------------------------------------------------------------------------------------
 
 # ------------ 1256-0224 (Poster in SED)----------------
 # Read in as pandas dataframe
-df = pd.read_csv('Redone/best1256-0224 (L3.5sd) SED.txt',sep=" ", header = 1, names=["w", "f", "err"])
-df2 = pd.read_csv('Redone/best1256-0224 (L3.5sd) phot.txt',sep=" ", header = 1, names=["w", "f", "err"])
+df = pd.read_csv('Redone/best1256-0224 (L3.5sd) SED.txt', sep=" ", header=1, names=["w", "f", "err"])
+df2 = pd.read_csv('Redone/best1256-0224 (L3.5sd) phot.txt', sep=" ", header=1, names=["w", "f", "err"])
 
 # TODO: Add in comparison objects and plot
 # ------------- Comparison Objects -------------------
 # No problem spectra for 1626+3925
 w_1626, f_1626, unc_1626 = np.loadtxt('Redone/MIR(2)1626+3925 (L4sd) SED.txt', delimiter=' ', unpack=True)
 phot1626_w, phot1626_f, phot1626_err = np.loadtxt('Redone/MIR(2)1626+3925 (L4sd) phot.txt', delimiter=' ',
-                                                  usecols=(1,2,3), unpack=True)
+                                                  usecols=(1, 2, 3), unpack=True)
 # ---------------------- Same Teff ----------------------------------
 
 w_0036, f_0036, u_0036 = np.loadtxt('Redone/0036+1821 (L3.5) SED.txt', delimiter=' ', unpack=True)
-wp_0036, fp_0036, up_0036 = np.loadtxt('Redone/0036+1821 (L3.5) phot.txt', delimiter= ' ', usecols=(1,2,3),unpack=True)
+wp_0036, fp_0036, up_0036 = np.loadtxt('Redone/0036+1821 (L3.5) phot.txt', delimiter=' ',
+                                       usecols=(1, 2, 3), unpack=True)
 # 1207
 
 # ---------------- Same Lbol -------------------------------------
-#0501
-#0153
+# 0501
+# 0153
 
 
 # -------- Generate labeled spectra plot ---------------------------
@@ -41,21 +42,21 @@ ax1.loglog(opt['w'], opt['f'], c='#0179FF')
 ax1.loglog(overlap['w'], overlap['f'], c='#009B45', lw=5, alpha=0.5)
 ax1.loglog(overlap['w'], overlap['f'], c='#0179FF', alpha=0.8)  # alpha=0.3 for transparency
 ax1.loglog(nir['w'], nir['f'], c='#009B45')
-ax1.scatter(df2['w'][0], df2['f'][0],  c='#f768a1',s=100)
+ax1.scatter(df2['w'][0], df2['f'][0],  c='#f768a1', s=100)
 ax1.errorbar(df2['w'][0], df2['f'][0], yerr=df2['err'][0], c='#f768a1')
-ax1.scatter(df2['w'][1], df2['f'][1],  c='#c51b8a',s=100)
+ax1.scatter(df2['w'][1], df2['f'][1],  c='#c51b8a', s=100)
 ax1.errorbar(df2['w'][1], df2['f'][1], yerr=df2['err'][1], c='#c51b8a')
-ax1.scatter(df2['w'][2], df2['f'][2],  c='#c51b8a',s=100)
+ax1.scatter(df2['w'][2], df2['f'][2],  c='#c51b8a', s=100)
 ax1.errorbar(df2['w'][2], df2['f'][2], yerr=df2['err'][2], c='#c51b8a')
-ax1.scatter(df2['w'][3], df2['f'][3],  c='#7a0177',s=100)
+ax1.scatter(df2['w'][3], df2['f'][3],  c='#7a0177', s=100)
 ax1.errorbar(df2['w'][3], df2['f'][3], yerr=df2['err'][3], c='#7a0177')
-ax1.scatter(df2['w'][4], df2['f'][4],  c='#7a0177',s=100)
+ax1.scatter(df2['w'][4], df2['f'][4],  c='#7a0177', s=100)
 ax1.errorbar(df2['w'][4], df2['f'][4], yerr=df2['err'][4], c='#7a0177')
 
 
 # ---- Removing some of the junk in K and H Bands ***THIS IS THE BETTER PLOT ------------------
 h = df[(df['w'] >= 1.48933) & (df['w'] <= 1.8)]
-k= df[(df['w'] >= 2.0175)]
+k = df[(df['w'] >= 2.0175)]
 zj = df[(df['w'] >= 0.950328) & (df['w'] <= 1.3500)]
 
 fig = plt.figure()
@@ -66,15 +67,15 @@ ax1.loglog(overlap['w'], overlap['f'], c='#0179FF', alpha=0.8)  # alpha=0.3 for 
 ax1.loglog(zj['w'], zj['f'], c='#009B45')
 ax1.loglog(h['w'], h['f'], c='#009B45')
 ax1.loglog(k['w'], k['f'], c='#009B45')
-ax1.scatter(df2['w'][0], df2['f'][0],  c='#f768a1',s=100)
+ax1.scatter(df2['w'][0], df2['f'][0],  c='#f768a1', s=100)
 ax1.errorbar(df2['w'][0], df2['f'][0], yerr=df2['err'][0], c='#f768a1')
-ax1.scatter(df2['w'][1], df2['f'][1],  c='#c51b8a',s=100)
+ax1.scatter(df2['w'][1], df2['f'][1],  c='#c51b8a', s=100)
 ax1.errorbar(df2['w'][1], df2['f'][1], yerr=df2['err'][1], c='#c51b8a')
-ax1.scatter(df2['w'][2], df2['f'][2],  c='#c51b8a',s=100)
+ax1.scatter(df2['w'][2], df2['f'][2],  c='#c51b8a', s=100)
 ax1.errorbar(df2['w'][2], df2['f'][2], yerr=df2['err'][2], c='#c51b8a')
-ax1.scatter(df2['w'][3], df2['f'][3],  c='#7a0177',s=100)
+ax1.scatter(df2['w'][3], df2['f'][3],  c='#7a0177', s=100)
 ax1.errorbar(df2['w'][3], df2['f'][3], yerr=df2['err'][3], c='#7a0177')
-ax1.scatter(df2['w'][4], df2['f'][4],  c='#7a0177',s=100)
+ax1.scatter(df2['w'][4], df2['f'][4],  c='#7a0177', s=100)
 ax1.errorbar(df2['w'][4], df2['f'][4], yerr=df2['err'][4], c='#7a0177')
 
 # -------- Generate plots ---------------------------
