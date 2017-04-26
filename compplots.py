3748ed947a2c36bd008fff0cd04fe8bb00737067
ECGonzales <gonza338@gmail.com>
2017-04-26 12:29:49 -0400

    testing remote changes

--- compplots.py
+++ compplots.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# making a chnage to test new remotes!
 # ------------------------------------------------------------------------------------
 # ------------------- Read in Spectra and Photometry files ---------------------------
 # -------------------------------------------------------------------------------------
