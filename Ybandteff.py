1b9ab0807a9cdec256f7b3d2688d483a4974c9e9
ECGonzales <gonza338@gmail.com>
2017-05-12 13:31:12 -0400

    Removed unused import

--- Ybandteff.py
+++ Ybandteff.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import ScalarFormatter
 
 # ------------------------------------------------------------------------------------
 # ------------------- Read in Spectra and Photometry files ---------------------------
