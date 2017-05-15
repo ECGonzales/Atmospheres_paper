c91e635eb4c47cc83da8782dd17fc81a9874124f
ECGonzales <gonza338@gmail.com>
2017-05-15 16:44:34 -0400

    Finalized plots for each region with features labeled with horizontal and vertical lines.

--- HbandTeff.py
+++ HbandTeff.py
@@ -14,28 +14,31 @@ df_young = pd.read_csv('Data/teff0223-5815 (L0gamma) SED.txt', sep=" ", comment=
                        names=["w", "f", "err"])
 df_field = pd.read_csv('Data/0036+1821 (L3.5) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 
+# -------- Remove lines from when trimming with SEDkit for 1256
+df_1256h = df_1256[(df_1256['w'] >= 1.49786) & (df_1256['w'] <= 1.79593)]
+
 # -------------------------------------------------------------------------------------
 # ------------------------- Normalize the spectra -------------------------------------
 # -------------------------------------------------------------------------------------
 # Determine region good for all spectra to take the average flux over
-norm_region = df_1256[(df_1256['w'] >= 0.98) & (df_1256['w'] <= 0.988)]
-norm_df_1256 = df_1256['f']/(np.average(norm_region['f']))
+norm_region = df_1256h[(df_1256h['w'] >= 1.5) & (df_1256h['w'] <= 1.52)]
+norm_df_1256 = df_1256h['f']/(np.average(norm_region['f']))
 
-norm_region2 = df_young[(df_young['w'] >= 0.98) & (df_young['w'] <= 0.988)]
+norm_region2 = df_young[(df_young['w'] >= 1.5) & (df_young['w'] <= 1.52)]
 norm_df_young = df_young['f']/(np.average(norm_region2['f']))
 
-norm_region3 = df_field[(df_field['w'] >= 0.98) & (df_field['w'] <= 0.988)]
+norm_region3 = df_field[(df_field['w'] >= 1.5) & (df_field['w'] <= 1.52)]
 norm_df_field = df_field['f']/(np.average(norm_region3['f']))
 
 # -------------------------------------------------------------------------------------
-# ------------------- Plotting: Y band comparison -------------------------------
+# ------------------- Plotting: H band comparison -------------------------------
 # -------------------------------------------------------------------------------------
 # ------ Set up figure layout --------
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 fig.set_size_inches(10, 8)
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
-plt.xlim([0.95, 1.10])
+plt.xlim([1.42, 1.80])
 plt.ylim([0, 3.5])
 
 # ------Tick size and Axes Labels --------
@@ -45,41 +48,29 @@ plt.xlabel('Wavelength ($\mu m$)', fontsize=25)
 plt.ylabel('Normalized Flux (F$_\lambda$)', fontsize=25)
 
 # -------- Add data -----------
-ax1.plot(df_1256['w'], norm_df_1256, c='blue')
+ax1.plot(df_1256h['w'], norm_df_1256, c='blue')
 ax1.plot(df_field['w'], norm_df_field + 0.5, c='#7C7D70')
 ax1.plot(df_young['w'], norm_df_young + 1, c='#D01810')
 
 # ------- Label Features --------------------------
-# --- To make line for feature
-FeH1 = pd.DataFrame()
-FeH1['x'] = [0.9896, 1.0]
-FeH1['y'] = [2.4, 2.4]
-plt.plot(FeH1['x'], FeH1['y'], color='k')
-ax1.text(0.272, 0.69, 'FeH', transform=ax1.transAxes, color='k', fontsize=15)
-# -- To make a vertical line
-FeH1d = pd.DataFrame()
-FeH1d['x'] = [0.9896, 0.9896]
-FeH1d['y'] = [2.3, 2.4]
-plt.plot(FeH1d['x'], FeH1d['y'], color='k')
+FeH = pd.DataFrame()
+FeH['x'] = [1.581, 1.66]
+FeH['y'] = [2.6, 2.6]
+plt.plot(FeH['x'], FeH['y'], color='k')
+ax1.text(0.5, 0.75, 'FeH', transform=ax1.transAxes, color='k', fontsize=15)
+FeHd = pd.DataFrame()
+FeHd['x'] = [1.581, 1.581]
+FeHd['y'] = [2.5, 2.6]
+plt.plot(FeHd['x'], FeHd['y'], color='k')
 
-FeH2 = pd.DataFrame()
-FeH2['x'] = [0.998, 1.085]
-FeH2['y'] = [2.85, 2.85]
-plt.plot(FeH2['x'], FeH2['y'], color='k')
-ax1.text(0.55, 0.82, 'FeH', transform=ax1.transAxes, color='k', fontsize=15)
-FeH2d = pd.DataFrame()
-FeH2d['x'] = [0.998, 0.998]
-FeH2d['y'] = [2.7, 2.85]
-plt.plot(FeH2d['x'], FeH2d['y'], color='k')
+CH4 = pd.DataFrame()
+CH4['x'] = [1.67, 1.75]
+CH4['y'] = [2.8, 2.8]
+plt.plot(CH4['x'], CH4['y'], color='k')
+ax1.text(0.74, 0.81, 'CH$_\mathrm{4}$', transform=ax1.transAxes, color='k', fontsize=15)
+CH4d = pd.DataFrame()
+CH4d['x'] = [1.67, 1.67]
+CH4d['y'] = [2.6, 2.8]
+plt.plot(CH4d['x'], CH4d['y'], color='k')
 
-H2O = pd.DataFrame()
-H2O['x'] = [1.08, 1.099]
-H2O['y'] = [3.1, 3.1]
-plt.plot(H2O['x'], H2O['y'], color='k')
-ax1.text(0.9, 0.9, 'H$_\mathrm{2} $O', transform=ax1.transAxes, color='k', fontsize=15)
-H2Od = pd.DataFrame()
-H2Od['x'] = [1.08, 1.08]
-H2Od['y'] = [2.95, 3.1]
-plt.plot(H2Od['x'], H2Od['y'], color='k')
-
-plt.savefig('Plots/YbandTeff.png')
+plt.savefig('Plots/HbandTeff.png')
--- KbandTeff.py
+++ KbandTeff.py
@@ -14,28 +14,31 @@ df_young = pd.read_csv('Data/teff0223-5815 (L0gamma) SED.txt', sep=" ", comment=
                        names=["w", "f", "err"])
 df_field = pd.read_csv('Data/0036+1821 (L3.5) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 
+# -------- Remove lines from when triming with SEDkit for 1256
+df_1256k = df_1256[(df_1256['w'] >= 2.051515)]
+
 # -------------------------------------------------------------------------------------
 # ------------------------- Normalize the spectra -------------------------------------
 # -------------------------------------------------------------------------------------
 # Determine region good for all spectra to take the average flux over
-norm_region = df_1256[(df_1256['w'] >= 0.98) & (df_1256['w'] <= 0.988)]
-norm_df_1256 = df_1256['f']/(np.average(norm_region['f']))
+norm_region = df_1256k[(df_1256k['w'] >= 2.16) & (df_1256k['w'] <= 2.20)]
+norm_df_1256 = df_1256k['f']/(np.average(norm_region['f']))
 
-norm_region2 = df_young[(df_young['w'] >= 0.98) & (df_young['w'] <= 0.988)]
+norm_region2 = df_young[(df_young['w'] >= 2.16) & (df_young['w'] <= 2.20)]
 norm_df_young = df_young['f']/(np.average(norm_region2['f']))
 
-norm_region3 = df_field[(df_field['w'] >= 0.98) & (df_field['w'] <= 0.988)]
+norm_region3 = df_field[(df_field['w'] >= 2.16) & (df_field['w'] <= 2.20)]
 norm_df_field = df_field['f']/(np.average(norm_region3['f']))
 
 # -------------------------------------------------------------------------------------
-# ------------------- Plotting: Y band comparison -------------------------------
+# ------------------- Plotting: K band comparison -------------------------------
 # -------------------------------------------------------------------------------------
 # ------ Set up figure layout --------
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 fig.set_size_inches(10, 8)
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
-plt.xlim([0.95, 1.10])
+plt.xlim([2.0, 2.35])
 plt.ylim([0, 3.5])
 
 # ------Tick size and Axes Labels --------
@@ -45,41 +48,25 @@ plt.xlabel('Wavelength ($\mu m$)', fontsize=25)
 plt.ylabel('Normalized Flux (F$_\lambda$)', fontsize=25)
 
 # -------- Add data -----------
-ax1.plot(df_1256['w'], norm_df_1256, c='blue')
-ax1.plot(df_field['w'], norm_df_field + 0.5, c='#7C7D70')
-ax1.plot(df_young['w'], norm_df_young + 1, c='#D01810')
+ax1.plot(df_1256k['w'], norm_df_1256, c='blue')
+ax1.plot(df_field['w'], norm_df_field + 1.2, c='#7C7D70')
+ax1.plot(df_young['w'], norm_df_young + 1.7, c='#D01810')
 
 # ------- Label Features --------------------------
-# --- To make line for feature
-FeH1 = pd.DataFrame()
-FeH1['x'] = [0.9896, 1.0]
-FeH1['y'] = [2.4, 2.4]
-plt.plot(FeH1['x'], FeH1['y'], color='k')
-ax1.text(0.272, 0.69, 'FeH', transform=ax1.transAxes, color='k', fontsize=15)
-# -- To make a vertical line
-FeH1d = pd.DataFrame()
-FeH1d['x'] = [0.9896, 0.9896]
-FeH1d['y'] = [2.3, 2.4]
-plt.plot(FeH1d['x'], FeH1d['y'], color='k')
+CIA_H2O = pd.DataFrame()
+CIA_H2O['x'] = [2.01, 2.34]
+CIA_H2O['y'] = [3.2, 3.2]
+plt.plot(CIA_H2O['x'], CIA_H2O['y'], color='k')
+ax1.text(0.45, 0.925, 'CIA H$_\mathrm{2} $O', transform=ax1.transAxes, color='k', fontsize=15)
 
-FeH2 = pd.DataFrame()
-FeH2['x'] = [0.998, 1.085]
-FeH2['y'] = [2.85, 2.85]
-plt.plot(FeH2['x'], FeH2['y'], color='k')
-ax1.text(0.55, 0.82, 'FeH', transform=ax1.transAxes, color='k', fontsize=15)
-FeH2d = pd.DataFrame()
-FeH2d['x'] = [0.998, 0.998]
-FeH2d['y'] = [2.7, 2.85]
-plt.plot(FeH2d['x'], FeH2d['y'], color='k')
+CO = pd.DataFrame()
+CO['x'] = [2.295, 2.34]
+CO['y'] = [2.95, 2.95]
+plt.plot(CO['x'], CO['y'], color='k')
+ax1.text(0.88, 0.85, 'CO', transform=ax1.transAxes, color='k', fontsize=15)
+COd = pd.DataFrame()
+COd['x'] = [2.295, 2.295]
+COd['y'] = [2.8, 2.95]
+plt.plot(COd['x'], COd['y'], color='k')
 
-H2O = pd.DataFrame()
-H2O['x'] = [1.08, 1.099]
-H2O['y'] = [3.1, 3.1]
-plt.plot(H2O['x'], H2O['y'], color='k')
-ax1.text(0.9, 0.9, 'H$_\mathrm{2} $O', transform=ax1.transAxes, color='k', fontsize=15)
-H2Od = pd.DataFrame()
-H2Od['x'] = [1.08, 1.08]
-H2Od['y'] = [2.95, 3.1]
-plt.plot(H2Od['x'], H2Od['y'], color='k')
-
-plt.savefig('Plots/YbandTeff.png')
+plt.savefig('Plots/KbandTeff.png')
