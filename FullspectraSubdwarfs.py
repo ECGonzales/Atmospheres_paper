b6d1307e75c1e4efaf0726056310d024e7bba5b9
eileengonzales <gonza338@gmail.com>
2017-08-04 17:08:00 -0400

    Remade full spectra and red optical with new teffs.

--- FullspectraSubdwarfs.py
+++ FullspectraSubdwarfs.py
@@ -6,7 +6,7 @@ import numpy as np
 # ------------------- Read in Spectra and Photometry files ---------------------------
 # ------------------------------------------------------------------------------------
 # Read  all in as pandas dataframes
-# None for 1256-1408, and problem for 0616
+# None for 1256-1408, and 1425 can't be normalized in this region, choosing to show HD instead.
 
 df_1256 = pd.read_csv('Data/correctpi1256-0224 (L3.5sd) SED.txt', sep=" ", comment='#', header=None,
                       names=["w", "f", "err"])
@@ -17,7 +17,7 @@ df_0616 = pd.read_csv('Data/0616-6407 (L5sd) SED.txt', sep=" ", comment='#', hea
 df_1013 = pd.read_csv('Data/1013-1356 (-) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 # df_125614 = pd.read_csv('', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 df_HD = pd.read_csv('Data/HD114762B (M9sd) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
-df_1425 = pd.read_csv('Data/1425+7102 (M8sd) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
+# df_1425 = pd.read_csv('Data/1425+7102 (M8sd) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 df_LHS = pd.read_csv('Data/1439+1839 (M7sd) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 df_1444 = pd.read_csv('Data/1444-2019 (M9sd) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
 df_1610 = pd.read_csv('Data/1610-0040 (M7sd) SED.txt', sep=" ", comment='#', header=None, names=["w", "f", "err"])
@@ -28,9 +28,6 @@ df_2036 = pd.read_csv('Data/2036+5059 (M7.5sd) SED.txt', sep=" ", comment='#', h
 # ---------------------- Remove Tails ------------------------------------------------
 # -------------------------------------------------------------------------------------
 df_HD = df_HD[(df_HD['w'] > 0.91) & (df_HD['w'] <= 3)]
-
-# Trim connecting line between opt and MIR
-df_1425= df_1425[(df_1425['w'] <= 0.92)]
 
 # -------------------------------------------------------------------------------------
 # --------- Normalize the spectra to same as before for 1256 --------------------------
@@ -57,8 +54,8 @@ norm_df_1013 = df_1013['f']/(np.average(norm_region_1013['f']))
 norm_region_LHS = df_LHS[(df_LHS['w'] >= 0.98) & (df_LHS['w'] <= 0.988)]
 norm_df_LHS = df_LHS['f']/(np.average(norm_region_LHS['f']))
 
-norm_region_1425 = df_1425[(df_1425['w'] >= 0.90) & (df_1425['w'] <= 0.91)]
-norm_df_1425 = df_1425['f']/(np.average(norm_region_1425['f']))
+# norm_region_1425 = df_1425[(df_1425['w'] >= 0.90) & (df_1425['w'] <= 0.91)] region that works fro 1425 but not HD
+# norm_df_1425 = df_1425['f']/(np.average(norm_region_1425['f']))
 
 norm_region_1610 = df_1610[(df_1610['w'] >= 0.98) & (df_1610['w'] <= 0.988)]
 norm_df_1610 = df_1610['f']/(np.average(norm_region_1610['f']))
@@ -93,16 +90,16 @@ plt.ylabel('Normalized Flux  ($F_\lambda$)', fontsize=25)
 
 # -------- Add data -----------
 ax1.plot(df_0532['w'], norm_df_0532, c='indigo')                                # sdL7 1647
-ax1.plot(df_0616['w'], norm_df_0616 + 2, c='darkviolet')                        # sdL5 ???
-ax1.plot(df_1626['w'], norm_df_1626 + 3, c='#531CF7')                         # sdL4 2158
+ax1.plot(df_0616['w'], norm_df_0616 + 2, c='darkviolet')                        # sdL5 1668
+ax1.plot(df_1626['w'], norm_df_1626 + 3, c='#531CF7')                           # sdL4 2158
 ax1.plot(df_1444['w'], norm_df_1444 + 4, c='mediumblue')                        # sdM9 2303
-ax1.plot(df_1256['w'], norm_df_1256 + 5, c='k')                                 # sdL3.5  2338
+ax1.plot(df_1256['w'], norm_df_1256 + 5, c='k')                                 # sdL3.5  2344
 # ax1.plot(df_1013['w'], norm_df_1013 + 5, c='#015DF7')                         # sdM9.5 2457
 # ax1.plot(df_LHS['w'], norm_df_LHS + 6, c='#01A1D6')                           # sdM7 2748
-# # ax1.plot(df_1425['w'], norm_df_1425, c='#09D5D6')                             # sdM8 2823      only MIR spectra
-# ax1.plot(df_1610['w'], norm_df_1610 + 7, c='#04A57F')                           # sdM7 2852
+# # ax1.plot(df_1425['w'], norm_df_1425, c='#09D5D6')                           # sdM8 2822      only MIR spectra
 # ax1.plot(df_HD['w'], norm_df_HD + 8, c='#09D67E')                             # sd--IRM9 2859
-# ax1.plot(df_2036['w'], norm_df_2036 + 9, c='#F7BE0F')                         # sdM7.5 3049
+# ax1.plot(df_1610['w'], norm_df_1610 + 7, c='#04A57F')                         # sdM7 2878
+# ax1.plot(df_2036['w'], norm_df_2036 + 9, c='#F7BE0F')                         # sdM7.5 3021
 # # ax1.plot(df_125614['w'], norm_df_125614, c='#C56201')                         # sdM8
 
 # ------- Label Sources -------------
@@ -135,24 +132,24 @@ plt.xlabel('Wavelength ($\mu m$)', fontsize=25)
 plt.ylabel('Normalized Flux  ($F_\lambda$)', fontsize=25)
 
 # -------- Add data -----------
-ax1.plot(df_1013['w'], norm_df_1013 , c='#015DF7')                         # sdM9.5 2457
-ax1.plot(df_LHS['w'], norm_df_LHS + 1, c='#01A1D6')                           # sdM7 2748
-ax1.plot(df_1425['w'], norm_df_1425 +2, c='#09D5D6')                             # sdM8 2822 ---
-ax1.plot(df_HD['w'], norm_df_HD + 3, c='#09D67E')                             # sd--IRM9 2859
-ax1.plot(df_1610['w'], norm_df_1610 + 2, c='#04A57F')                           # sdM7 2878
-ax1.plot(df_2036['w'], norm_df_2036 + 4, c='#F7BE0F')                         # sdM7.5 3021
+ax1.plot(df_1013['w'], norm_df_1013, c='#015DF7')                         # sdM9.5 2457
+ax1.plot(df_LHS['w'], norm_df_LHS + 1, c='#01A1D6')                       # sdM7 2748
+# ax1.plot(df_1425['w'], norm_df_1425 + 2, c='#09D5D6')                   # sdM8 2822
+ax1.plot(df_HD['w'], norm_df_HD + 2, c='#09D67E')                         # sd--IRM9 2859
+ax1.plot(df_1610['w'], norm_df_1610 + 3, c='#04A57F')                     # sdM7 2878
+ax1.plot(df_2036['w'], norm_df_2036 + 4, c='#F7BE0F')                     # sdM7.5 3021
 # ax1.plot(df_125614['w'], norm_df_125614, c='#C56201')                         # sdM8
 
 # ------- Label Sources -------------
 ax1.text(0.72, 0.11, '1013-1356 (sdM9.5)', transform=ax1.transAxes, color='#015DF7', fontsize=12)
 ax1.text(0.72, 0.07, 'T$_\mathrm{eff}: 2457 \pm 124$ K', transform=ax1.transAxes, color='#015DF7', fontsize=12)
-ax1.text(0.72, 0.3, 'LHS 377 (sdM7)', transform=ax1.transAxes, color='#01A1D6',  fontsize=12)
-ax1.text(0.72, 0.26, 'T$_\mathrm{eff}: 2748 \pm 36$ K', transform=ax1.transAxes, color='#01A1D6', fontsize=12)
-ax1.text(0.72, 0.54, '1610-0040 (sdM7)', transform=ax1.transAxes, color='#04A57F', fontsize=12)
-ax1.text(0.72, 0.5, 'T$_\mathrm{eff}: 2850 \pm 20$ K', transform=ax1.transAxes, color='#04A57F', fontsize=12)
-ax1.text(0.7, 0.74, 'HD114762B (d/sdM9)', transform=ax1.transAxes, color='#09D67E', fontsize=12)
-ax1.text(0.72, 0.7, 'T$_\mathrm{eff}: 2859 \pm 50$ K', transform=ax1.transAxes, color='#09D67E', fontsize=12)
+ax1.text(0.72, 0.31, 'LHS 377 (sdM7)', transform=ax1.transAxes, color='#01A1D6',  fontsize=12)
+ax1.text(0.72, 0.27, 'T$_\mathrm{eff}: 2748 \pm 36$ K', transform=ax1.transAxes, color='#01A1D6', fontsize=12)
+ax1.text(0.7, 0.55, 'HD114762B (d/sdM9)', transform=ax1.transAxes, color='#09D67E', fontsize=12)
+ax1.text(0.72, 0.51, 'T$_\mathrm{eff}: 2859 \pm 50$ K', transform=ax1.transAxes, color='#09D67E', fontsize=12)
+ax1.text(0.72, 0.74, '1610-0040 (sdM7)', transform=ax1.transAxes, color='#04A57F', fontsize=12)
+ax1.text(0.72, 0.7, 'T$_\mathrm{eff}: 2878 \pm 20$ K', transform=ax1.transAxes, color='#04A57F', fontsize=12)
 ax1.text(0.7, 0.9, '2036+5059 (sdM7.5)', transform=ax1.transAxes, color='#F7BE0F', fontsize=12)
-ax1.text(0.72, 0.86, 'T$_\mathrm{eff}: 3049 \pm 108$ K', transform=ax1.transAxes, color='#F7BE0F',fontsize=12)
+ax1.text(0.72, 0.86, 'T$_\mathrm{eff}: 3021 \pm 102$ K', transform=ax1.transAxes, color='#F7BE0F',fontsize=12)
 
 plt.savefig('Plots/Subdwarfs_firstpannel.png')
--- Subdwarfs_redoptical.py
+++ Subdwarfs_redoptical.py
@@ -78,7 +78,7 @@ ax1 = fig.add_subplot(111)
 fig.set_size_inches(8, 11)  # to make sure proper size run entire code at once and change 8 to 6.45 to
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
 plt.xlim([0.6, 0.9])
-plt.ylim([-0.05, 9.5])
+plt.ylim([-0.05, 11])
 
 # ------ Axes Labels --------
 plt.xticks(fontsize=20)
@@ -88,12 +88,12 @@ plt.ylabel('Normalized Flux  ($F_\lambda$)', fontsize=25)
 
 # -------- Add data -----------
 ax1.plot(df_0532['w'], norm_df_0532, c='indigo')                                # sdL7 1647
-ax1.plot(df_0616['w'], norm_df_0616 + 1, c='darkviolet')                          # sdL5 ???
-ax1.plot(df_1626['w'], norm_df_1626 + 2, c='#531CF7')                               # sdL4 2158
+ax1.plot(df_0616['w'], norm_df_0616 + 1.35, c='darkviolet')                          # sdL5 1668
+ax1.plot(df_1626['w'], norm_df_1626 + 2.3, c='#531CF7')                               # sdL4 2158
 ax1.plot(df_1444['w'], norm_df_1444 + 3, c='mediumblue')                            # sdM9 2303
 ax1.plot(df_1256['w'], norm_df_1256 + 4, c='k')                                     # sdL3.5  2338
 ax1.plot(df_1013['w'], norm_df_1013 + 5, c='#015DF7')                               # sdM9.5 2457
-ax1.plot(df_LHS['w'], norm_df_LHS + 6, c='#01A1D6')                                 # sdM7 2824
+ax1.plot(df_LHS['w'], norm_df_LHS + 6, c='#01A1D6')                                 # sdM7 2748
 ax1.plot(df_1425['w'], norm_df_1425 + 7, c='#09D5D6')                               # sdM8 2822
 # ax1.plot(df_HD['w'], norm_df_HD + 7, c='#09D67E')                                 # sd--IRM9 2859
 ax1.plot(df_1610['w'], norm_df_1610 + 8, c='#04A57F')                               # sdM7 2878
@@ -104,91 +104,97 @@ ax1.plot(df_2036['w'], norm_df_2036 + 9, c='#F7BE0F')                           
 # ------- Label Sources -------------
 ax1.text(0, 0.04, '0532+8246 (sdL7) T$_\mathrm{eff}: 1647 \pm 42$ K ', transform=ax1.transAxes, color='indigo',
          fontsize=12)
-ax1.text(0, 0.16, '0616-6407 (sdL5) T$_\mathrm{eff}: 1668 \pm 284$ K ', transform=ax1.transAxes, color='darkviolet',
+ax1.text(0, 0.175, '0616-6407 (sdL5) T$_\mathrm{eff}: 1668 \pm 284$ K ', transform=ax1.transAxes, color='darkviolet',
          fontsize=12)
-ax1.text(0, 0.27, '1626+3925 (sdL4) T$_\mathrm{eff}: 2158 \pm 41$ K', transform=ax1.transAxes, color='#531CF7',
+ax1.text(0, 0.26, '1626+3925 (sdL4) T$_\mathrm{eff}: 2158 \pm 41$ K', transform=ax1.transAxes, color='#531CF7',
          fontsize=12)
-ax1.text(0, 0.39, '1444-2019 (sdM9) T$_\mathrm{eff}: 2303 \pm 67$ K', transform=ax1.transAxes, color='mediumblue',
+ax1.text(0, 0.34, '1444-2019 (sdM9) T$_\mathrm{eff}: 2303 \pm 67$ K', transform=ax1.transAxes, color='mediumblue',
          fontsize=12)
-ax1.text(0, 0.48, '1256-0224 (sdL3.5) T$_\mathrm{eff}: 2344 \pm 314$ K', transform=ax1.transAxes, color='k',
+ax1.text(0, 0.42, '1256-0224 (sdL3.5) T$_\mathrm{eff}: 2344 \pm 314$ K', transform=ax1.transAxes, color='k',
          fontsize=12)
-ax1.text(0, 0.6, '1013-1356 (sdM9.5) T$_\mathrm{eff}: 2457 \pm 124$ K', transform=ax1.transAxes, color='#015DF7',
+ax1.text(0, 0.52, '1013-1356 (sdM9.5) T$_\mathrm{eff}: 2457 \pm 124$ K', transform=ax1.transAxes, color='#015DF7',
          fontsize=12)
-ax1.text(0, 0.77, 'LHS 377 (sdM7) T$_\mathrm{eff}: 2824 \pm 37$ K', transform=ax1.transAxes, color='#01A1D6',
+ax1.text(0, 0.615, 'LHS 377 (sdM7) T$_\mathrm{eff}: 2748 \pm 36$ K', transform=ax1.transAxes, color='#01A1D6',
          fontsize=12)
-ax1.text(0, 0.82, '1610-0040 (sdM7) T$_\mathrm{eff}: 2878 \pm 20$ K', transform=ax1.transAxes, color='#04A57F',
+ax1.text(0, 0.71, '1425+7102 (sdM8) T$_\mathrm{eff}: 2822 \pm 60$ K', transform=ax1.transAxes, color='#09D5D6',
          fontsize=12)
-ax1.text(0, 0.92, '2036+5059 (sdM7.5) T$_\mathrm{eff}: 3021 \pm 102$ K', transform=ax1.transAxes, color='#F7BE0F',
+ax1.text(0, 0.79, '1610-0040 (sdM7) T$_\mathrm{eff}: 2878 \pm 20$ K', transform=ax1.transAxes, color='#04A57F',
+         fontsize=12)
+ax1.text(0, 0.97, '2036+5059 (sdM7.5) T$_\mathrm{eff}: 3021 \pm 102$ K', transform=ax1.transAxes, color='#F7BE0F',
          fontsize=12)
 
 # ---- Label Features ------
 CaH2 = pd.DataFrame()
 CaH2['x'] = [0.635, 0.64]
-CaH2['y'] = [6.65, 6.65]
+CaH2['y'] = [9.5, 9.5]
 ax1.plot(CaH2['x'], CaH2['y'], color='k')
 CaHd2 = pd.DataFrame()
 CaHd2['x'] = [0.635, 0.635]
-CaHd2['y'] = [6.45, 6.65]
+CaHd2['y'] = [9.3, 9.5]
 ax1.plot(CaHd2['x'], CaHd2['y'], color='k')
-ax1.text(0.11, 0.705, 'CaH', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('CaH', xy=(0.63, 9.55), color='k', fontsize=12)
 
 CaI = pd.DataFrame()
-CaI['x'] = [0.658, 0.658]
-CaI['y'] = [6.5, 6.65]
+CaI['x'] = [0.657, 0.657]
+CaI['y'] = [9.4, 9.55]
 ax1.plot(CaI['x'], CaI['y'], color='k')
-ax1.text(0.17, 0.705, 'Ca$\,$I', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('Ca$\,$I', xy=(0.651, 9.6), color='k', fontsize=12)
 
 TiO2 = pd.DataFrame()
 TiO2['x'] = [0.658, 0.685]
-TiO2['y'] = [6.9, 6.9]
+TiO2['y'] = [10, 10]
 ax1.plot(TiO2['x'], TiO2['y'], color='k')
-ax1.text(0.223, 0.73, 'TiO', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('TiO', xy=(0.665, 10.05), color='k', fontsize=12)
 
 CaH = pd.DataFrame()
 CaH['x'] = [0.676, 0.705]
-CaH['y'] = [6.65, 6.65]
+CaH['y'] = [9.55, 9.55]
 ax1.plot(CaH['x'], CaH['y'], color='k')
 CaHd = pd.DataFrame()
 CaHd['x'] = [0.676, 0.676]
-CaHd['y'] = [6.45, 6.65]
+CaHd['y'] = [9.3, 9.55]
 ax1.plot(CaHd['x'], CaHd['y'], color='k')
-ax1.text(0.27, 0.705, 'CaH', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('CaH', xy=(0.675, 9.56), color='k', fontsize=12)
 
 TiO = pd.DataFrame()
 TiO['x'] = [0.705, 0.72]
-TiO['y'] = [6.75, 6.755]
+TiO['y'] = [9.75, 9.755]
 ax1.plot(TiO['x'], TiO['y'], color='k')
 TiOd = pd.DataFrame()
 TiOd['x'] = [0.705, 0.705]
-TiOd['y'] = [6.61, 6.75]
+TiOd['y'] = [9.61, 9.75]
 ax1.plot(TiOd['x'], TiOd['y'], color='k')
-ax1.text(0.36, 0.715, 'TiO', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('TiO', xy=(0.704, 9.8), color='k', fontsize=12)
 
 KII = pd.DataFrame()
 KII['x'] = [0.767, 0.767]
-KII['y'] = [6.7, 7]
+KII['y'] = [9.7, 10]
 ax1.plot(KII['x'], KII['y'], color='k')
-ax1.text(0.54, 0.75, 'K$\,$I', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('K$\,$I', xy=(0.761, 10.065), color='k', fontsize=12)
 
 Rb1 = pd.DataFrame()
 Rb1['x'] = [0.78, 0.78]
 Rb1['y'] = [4.4, 4.7]
 ax1.plot(Rb1['x'], Rb1['y'], color='k')
-ax1.text(0.58, 0.5, 'Rb$\,$I', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('Rb$\,$I', xy=(0.77, 4.75), color='k', fontsize=12)
 
 Rb = pd.DataFrame()
 Rb['x'] = [0.795, 0.795]
-Rb['y'] = [5.3, 5.5]
+Rb['y'] = [10, 10.3]
 ax1.plot(Rb['x'], Rb['y'], color='k')
-ax1.text(0.62, 0.53, 'Rb$\,$I', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('Rb$\,$I', xy=(0.79, 10.35), color='k', fontsize=12)
 
-ax1.text(0.71, 0.36, 'NaI', transform=ax1.transAxes, color='k', fontsize=12)
+NaI = pd.DataFrame()
+NaI['x'] = [0.818, 0.818]
+NaI['y'] = [10.1, 10.5]
+ax1.plot(NaI['x'], NaI['y'], color='k')
+ax1.annotate('NaI', xy=(0.81, 10.55), color='k', fontsize=12)
 
 TiI = pd.DataFrame()
 TiI['x'] = [0.844, 0.844]
-TiI['y'] = [7.4, 7.6]
+TiI['y'] = [9.4, 9.6]
 ax1.plot(TiI['x'], TiI['y'], color='k')
-ax1.text(0.79, 0.76, 'Ti$\,$I', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('Ti$\,$I', xy=(0.836, 9.1), color='k', fontsize=12)
 
 CrH = pd.DataFrame()
 CrH['x'] = [0.86, 0.865]
@@ -196,9 +202,9 @@ CrH['y'] = [5.4, 5.4]
 ax1.plot(CrH['x'], CrH['y'], color='k')
 CrHd = pd.DataFrame()
 CrHd['x'] = [0.86, 0.86]
-CrHd['y'] = [5.25, 5.4]
+CrHd['y'] = [5.2, 5.4]
 ax1.plot(CrHd['x'], CrHd['y'], color='k')
-ax1.text(0.84, 0.575, 'CrH', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('CrH', xy=(0.85, 5.45), color='k', fontsize=12)
 
 FeH = pd.DataFrame()
 FeH['x'] = [0.867, 0.875]
@@ -208,6 +214,6 @@ FeH = pd.DataFrame()
 FeH['x'] = [0.867, 0.867]
 FeH['y'] = [5, 5.2]
 ax1.plot(FeH['x'], FeH['y'], color='k')
-ax1.text(0.89, 0.553, 'FeH', transform=ax1.transAxes, color='k', fontsize=12)
+ax1.annotate('FeH', xy=(0.865, 5.25), color='k', fontsize=12)
 
 plt.savefig('Plots/Subdwarfs_redoptical_split.png')
