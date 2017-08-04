19e24cc5730becce610be696919c6d631ca2d9e6
eileengonzales <gonza338@gmail.com>
2017-08-04 17:45:52 -0400

    Added young masses

--- SptvMass.py
+++ SptvMass.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep=" ", comment='#', header=None,
+df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep="\s+", comment='#', header=None,
                      names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ', 'MJ_unc',
                             'MH', 'MH_unc', 'MK', 'MK_unc', 'MW1', 'MW1_unc', 'MW2', 'MW2_unc'])
-
+df_young = pd.read_csv('Data/young_masses.txt', sep="\s+", comment='#', header=None, names=['name', 'spt', 'mass',
+                                                                                            'mass_unc'])
 # -------------------------------------------------------------------------------------
 # ------------------------- Make Plot: Spt v mass ------------------------------------
 # -------------------------------------------------------------------------------------
