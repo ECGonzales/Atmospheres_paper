ed412a42b44480f2149838c5a4617cbd6b28ad22
eileengonzales <gonza338@gmail.com>
2017-08-07 12:36:57 -0400

    Added young and filed objects to plots

--- SptvMass.py
+++ SptvMass.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep="\s+", comment='#', header=None,
+df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep=" ", comment='#', header=None,
                      names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ', 'MJ_unc',
                             'MH', 'MH_unc', 'MK', 'MK_unc', 'MW1', 'MW1_unc', 'MW2', 'MW2_unc'])
-df_young = pd.read_csv('Data/young_masses.txt', sep="\s+", comment='#', header=None, names=['name', 'spt', 'mass',
-                                                                                            'mass_unc'])
+df_young = pd.read_csv('Data/young_masses.txt', sep="\s+", comment='#', header=None,
+                       names=['name', 'spt', 'mass', 'mass_unc', 'Teff', 'Teff_unc'])
+df_field = pd.read_csv('Data/field_masses.txt', sep="\t", comment='#', header=None,
+                       names=['name', 'spt', 'mass', 'mass_unc', 'Teff', 'Teff_unc'])
 # -------------------------------------------------------------------------------------
 # ------------------------- Make Plot: Spt v mass ------------------------------------
 # -------------------------------------------------------------------------------------
@@ -24,8 +26,12 @@ plt.xlabel('Spectral Type', fontsize=25)
 plt.ylabel('Mass (M$_\mathrm{J}$)', fontsize=25)
 
 # ----- Add data -----
-plt.scatter(df_sub['SpT'], df_sub['mass'], color='blue', s=100)
-ax1.errorbar(df_sub['SpT'], df_sub['mass'], yerr=df_sub['mass_unc'], c='blue', fmt='o')
+plt.scatter(df_field['spt'], df_field['mass'], color='#7C7D70')
+ax1.errorbar(df_field['spt'], df_field['mass'], yerr=df_field['mass_unc'], c='#7C7D70', fmt='o')
+plt.scatter(df_young['spt'], df_young['mass'], color='#D01810')
+ax1.errorbar(df_young['spt'], df_young['mass'], yerr=df_young['mass_unc'], c='#D01810', fmt='o')
+plt.scatter(df_sub['SpT'], df_sub['mass'], color='blue',s=100, zorder=5)
+ax1.errorbar(df_sub['SpT'], df_sub['mass'], yerr=df_sub['mass_unc'], c='blue', fmt='o', zorder=6)
 
 plt.savefig('Plots/SptVmass.png')
 
@@ -47,7 +53,11 @@ plt.xlabel('Mass (M$_\mathrm{J}$)', fontsize=25)
 plt.ylabel('T$_\mathrm{eff}$ (K)', fontsize=25)
 
 # ----- Add data -----
-plt.scatter(df_sub['Teff'], df_sub['mass'], color='blue', s=100)
-ax1.errorbar(df_sub['Teff'], df_sub['mass'], yerr=df_sub['mass_unc'], c='blue', fmt='o')
+plt.scatter(df_field['mass'], df_field['Teff'], color='#7C7D70')
+ax1.errorbar(df_field['mass'], df_field['Teff'], yerr=df_field['Teff_unc'], c='#7C7D70', fmt='o')
+plt.scatter(df_young['mass'], df_young['Teff'], color='#D01810')
+ax1.errorbar(df_young['mass'], df_young['Teff'], yerr=df_young['Teff_unc'], c='#D01810', fmt='o')
+plt.scatter(df_sub['mass'], df_sub['Teff'], color='blue', s=100, zorder=5)
+ax1.errorbar(df_sub['mass'], df_sub['Teff'], yerr=df_sub['Teff_err'], c='blue', fmt='o', zorder=6)
 
 plt.savefig('Plots/TeffVmass.png')
