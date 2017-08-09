4756c71211c25bad9e425545206594ebb04f0411
eileengonzales <gonza338@gmail.com>
2017-08-09 16:14:13 -0400

    Added fits and labeled 1256

--- SpTvTeff.py
+++ SpTvTeff.py
@@ -17,18 +17,18 @@ df_fld = df_comb[(df_comb['grav'] == 3)]
 df_young = df_comb[(df_comb['grav'] >= 1) & (df_comb['grav'] <= 2)]
 
 # -------------------------------------------------------------------------------------
-# ------------------------- Make Plot: Spt v Teff ------------------------------------
+# ------------------------- Make Plot: Spt v Teff -------------------------------------
 # -------------------------------------------------------------------------------------
 # ------ Set up figure layout --------
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 fig.set_size_inches(10, 6.45)  # to make sure proper size run entire code at once and change 8 to 6.45 to
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
-plt.xlim([5, 30])
+plt.xlim([5.5, 18.5])
 plt.ylim([500, 3200])
 
 # ------ Axes Labels --------
-plt.xticks([5, 10, 15, 20, 25, 30], ['M5', 'L0', 'L5', 'T0', 'T5', 'Y0'], fontsize=20)
+plt.xticks([6, 10, 15, 18], ['M6', 'L0', 'L5', 'L8'], fontsize=20)
 plt.yticks(fontsize=20)
 plt.xlabel('Spectral Type', fontsize=25)
 plt.ylabel('T$_\mathrm{eff}$ (K)', fontsize=25)
@@ -42,9 +42,60 @@ plt.scatter(df_sub['SpT'], df_sub['Teff'], color='blue', s=100, zorder=5)
 ax1.errorbar(df_sub['SpT'], df_sub['Teff'], yerr=df_sub['Teff_err'], c='blue', fmt='o', zorder=6)
 # zorder makes it go on top even if it covers other points!
 
+# --- Designate 1256-0224 -----
+plt.scatter(df_sub['SpT'][0], df_sub['Teff'][0], color='blue', s=500, zorder=7, marker="*")
+ax1.annotate('1256-0224', xy=(12.7, 2700), color='k', fontsize=12)
+
+# -------------------------------------------------------------------------------------
+# ------------------------- Polynomial fits  -----------------------------------------
+# -------------------------------------------------------------------------------------
 # ------ Fit polynomial for subdwarfs ------
-# drop nan from coulmn need to get polynomial
+# drop nan from column need to get polynomial
 df_subpoly = df_sub[pd.notnull(df_sub['Teff'])]
-np.polyfit(df_subpoly['SpT'], df_subpoly['Teff'], 1)
+
+# --- Get uncertainites for upper and lower teff limits ----
+df_subpoly['Terr_up'] = df_subpoly['Teff'] + df_subpoly['Teff_err']
+df_subpoly['Terr_d'] = df_subpoly['Teff'] - df_subpoly['Teff_err']
+
+# ------ Fit the values --------
+coeffs = np.polyfit(df_subpoly['SpT'], df_subpoly['Teff'], 1)
+line = np.poly1d(coeffs)
+
+coeffs_up = np.polyfit(df_subpoly['SpT'], df_subpoly['Terr_up'], 1)
+line_up = np.poly1d(coeffs_up)
+
+coeffs_d = np.polyfit(df_subpoly['SpT'], df_subpoly['Terr_d'], 1)
+line_d = np.poly1d(coeffs_d)
+
+# ---- Plot the fit lines -----
+xp = np.linspace(5, 30, 100)
+plt.plot(xp, line(xp), '-', color='k')
+plt.plot(xp, line_up(xp), '-', color='#17becf', alpha=.25)
+plt.plot(xp, line_d(xp), '-', color='#17becf', alpha=.25)
+ax1.fill_between(xp, line_up(xp), line_d(xp), alpha=.25, color='#17becf')
 
 plt.savefig('Plots/SptvTeff.png')
+
+# From this example on curve fitting using the cov=True
+# https://stackoverflow.com/questions/28505008/numpy-polyfit-how-to-get-1-sigma-uncertainty-around-the-estimated-curve
+# Doesn't look much different
+# x= df_subpoly['SpT']
+# y = df_subpoly['Teff']
+# n = 1
+# p, C_p = np.polyfit(x, y, n, cov=True)  # C_z is estimated covariance matrix
+# # Do the interpolation for plotting:
+# t = np.linspace(5, 19, 500)
+# # Matrix with rows 1, t, t**2, ...:
+# TT = np.vstack([t**(n-i) for i in range(n+1)]).T
+# yi = np.dot(TT, p)  # matrix multiplication calculates the polynomial values
+# C_yi = np.dot(TT, np.dot(C_p, TT.T)) # C_y = TT*C_z*TT.T
+# sig_yi = np.sqrt(np.diag(C_yi))  # Standard deviations are sqrt of diagonal
+# # Do the plotting:
+# fg, ax = plt.subplots(1, 1)
+# ax.set_title("Fit for Polynomial (degree {}) with $\pm1\sigma$-interval".format(n))
+# ax.fill_between(t, yi+sig_yi, yi-sig_yi, alpha=.25)
+# ax.plot(t, yi,'-')
+# ax.plot(x, y, 'ro')
+# ax.axis('tight')
+# fg.canvas.draw()
+# plt.show()
--- SptvLbol.py
+++ SptvLbol.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep=" ", comment='#', header=None,
                      names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ', 'MJ_unc',
@@ -23,11 +24,11 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111)
 fig.set_size_inches(10, 6.45)  # to make sure proper size run entire code at once and change 8 to 6.45 to
 plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
-plt.xlim([5, 30])
+plt.xlim([5.5, 18.5])
 plt.ylim([-6, -2.3])
 
 # ------ Axes Labels --------
-plt.xticks([5, 10, 15, 20, 25, 30], ['M5', 'L0', 'L5', 'T0', 'T5', 'Y0'], fontsize=20)
+plt.xticks([6, 10, 15, 18], ['M6', 'L0', 'L5', 'L8'], fontsize=20)
 plt.yticks(fontsize=20)
 plt.xlabel('Spectral Type', fontsize=25)
 plt.ylabel('log(L$_\mathrm{bol}$/L$_\odot$)', fontsize=25)
@@ -39,5 +40,37 @@ plt.scatter(df_young['spt'], df_young['Lbol'], color='#D01810')
 ax1.errorbar(df_young['spt'], df_young['Lbol'], yerr=df_young['Lbol_err'], c='#D01810', fmt='o')
 plt.scatter(df_sub['SpT'], df_sub['lbol'], color='blue', s=100, zorder=5)
 ax1.errorbar(df_sub['SpT'], df_sub['lbol'], yerr=df_sub['lbol_err'], c='blue', fmt='o', zorder=6)
+
+# --- Designate 1256-0224 -----
+plt.scatter(df_sub['SpT'][0], df_sub['lbol'][0], color='blue', s=500, zorder=7, marker="*")
+ax1.annotate('1256-0224', xy=(12.7, -3.25), color='k', fontsize=12)
+
+# -------------------------------------------------------------------------------------
+# ------------------------- Polynomial fits  -----------------------------------------
+# -------------------------------------------------------------------------------------
+# ------ Fit polynomial for subdwarfs ------
+# drop nan from column need to get polynomial
+df_subpoly = df_sub[pd.notnull(df_sub['lbol'])]
+
+# --- Get uncertainites for upper and lower teff limits ----
+df_subpoly['Lbol_up'] = df_subpoly['lbol'] + df_subpoly['lbol_err']
+df_subpoly['Lbol_d'] = df_subpoly['lbol'] - df_subpoly['lbol_err']
+
+# ------ Fit the values --------
+coeffs = np.polyfit(df_subpoly['SpT'], df_subpoly['lbol'], 1)
+line = np.poly1d(coeffs)
+
+coeffs_up = np.polyfit(df_subpoly['SpT'], df_subpoly['Lbol_up'], 1)
+line_up = np.poly1d(coeffs_up)
+
+coeffs_d = np.polyfit(df_subpoly['SpT'], df_subpoly['Lbol_d'], 1)
+line_d = np.poly1d(coeffs_d)
+
+# ---- Plot the fit lines -----
+xp = np.linspace(5, 30, 100)
+plt.plot(xp, line(xp), '-', color='k')
+plt.plot(xp, line_up(xp), '-', color='#17becf', alpha=.25)
+plt.plot(xp, line_d(xp), '-', color='#17becf', alpha=.25)
+ax1.fill_between(xp, line_up(xp), line_d(xp), alpha=.25, color='#17becf')
 
 plt.savefig('Plots/SptvLbol.png')
