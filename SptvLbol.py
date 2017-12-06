637dc8ead97bd71ba4502bcb7b8c9db6bdfb8adf
eileengonzales <gonza338@gmail.com>
2017-12-06 05:11:26 -0300

    Added MCMC for fits

--- SptvLbol.py
+++ SptvLbol.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import emcee
+import pdb  # Use pdb.set_trace() to put a stop in the code like idl
+import corner
 
-df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep=" ", comment='#', header=None,
+# Set as stop for debugging
+stop = pdb.set_trace  # stop() to run
+
+# -------- Read in the data --------
+df_sub = pd.read_csv('Data/Subdwarf_Spt_v_Teff.txt', sep="\s+", comment='#', header=None,
                      names=["name", "SpT", "Teff", 'Teff_err', 'lbol', 'lbol_err', 'mass', 'mass_unc', 'MJ', 'MJ_unc',
                             'MH', 'MH_unc', 'MK', 'MK_unc', 'MW1', 'MW1_unc', 'MW2', 'MW2_unc'])
 df_comb = pd.read_csv('Data/Lbol+Teff-February2017_updated.txt', sep="\s+", comment='#', header=None,
@@ -15,6 +22,87 @@ df_comb.set_value(174, 'grav', 3)
 # ---- Split combined dataframe into field (group 3) and low g groups 1,2) ----------
 df_fld = df_comb[(df_comb['grav'] == 3)]
 df_young = df_comb[(df_comb['grav'] >= 1) & (df_comb['grav'] <= 2)]
+
+# -------------------------------------------------------------------------------------
+# ------------------------- Polynomial fits  -----------------------------------------
+# -------------------------------------------------------------------------------------
+# ------ Fit polynomial for subdwarfs ------
+# drop nan from column need to get polynomial
+df_subpoly = df_sub[pd.notnull(df_sub['lbol'])]
+
+# --- Get uncertainites for upper and lower teff limits ----
+df_subpoly['Lbol_up'] = df_subpoly['lbol'] + df_subpoly['lbol_err']
+df_subpoly['Lbol_d'] = df_subpoly['lbol'] - df_subpoly['lbol_err']
+
+# ------ Fit the values --------
+coeffs = np.polyfit(df_subpoly['SpT'], df_subpoly['lbol'], 1)
+line = np.poly1d(coeffs)
+
+coeffs_up = np.polyfit(df_subpoly['SpT'], df_subpoly['Lbol_up'], 1)
+line_up = np.poly1d(coeffs_up)
+
+coeffs_d = np.polyfit(df_subpoly['SpT'], df_subpoly['Lbol_d'], 1)
+line_d = np.poly1d(coeffs_d)
+
+# ---- print values to screen -------
+print coeffs
+print coeffs_up
+print coeffs_d
+
+# ---- Plot the fit lines -----
+# xp = np.linspace(5, 30, 100)
+# plt.plot(xp, line(xp), '-', color='k')
+# plt.plot(xp, line_up(xp), '-', color='#17becf', alpha=.25)
+# plt.plot(xp, line_d(xp), '-', color='#17becf', alpha=.25)
+# ax1.fill_between(xp, line_up(xp), line_d(xp), alpha=.25, color='#17becf')
+
+# -------------------------------------------------------------------------------------
+# ------------------------------ Use emcee to fit instead -----------------------------
+# -------------------------------------------------------------------------------------
+
+# Define the likelihood function: a line y=mx+b
+
+
+def lnprob(x, lbol, lbol_err, spt):
+    if x[2] < 0:
+        return -np.inf
+    model_line = x[0]*spt + x[1]
+    sigma_dm = (lbol - model_line)/np.sqrt(lbol_err**2+x[2]**2)  # Distance between the data points and the model line
+    return -(1./2.)*sum(np.log(x[2]**2+lbol_err**2))-(1./2.)*sum(sigma_dm**2)-np.log(x[2])  # np.log(x[2]) is an uninformative prior on the intrinsic dispersion
+                                                    # It favors large numbers less
+
+# Define the parameters we need to imput into the mcmc
+ndim = 3  # Number of parameters in my lnprob after the x
+nwalkers = 12
+nsteps = 1000  # this is a standard starting point of 1000
+
+parm_est = [-0.124, -2.089, 0]  # This is my estimate on the slope and y-intercept from my polyfits
+parm_scale = [0.012, 0.21, 0.21]  # the scatter about my estimated start points 10% for teff, 500 Degrees for K
+parm_scale_2d = np.array(parm_scale).reshape(1, ndim).repeat(nwalkers, axis=0)  # Create a 2D array of values with
+parm_est_2d = np.array(parm_est).reshape(1, ndim).repeat(nwalkers, axis=0)      # nwalkers rows
+pos = parm_est_2d + np.random.rand(nwalkers, ndim)*parm_scale_2d
+
+
+# Start the mcmc add .values to the pandas dataframe to convert to numpy array
+# Use the df_subpoly array that has removed the nans before can rum the mcmc
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(df_subpoly['lbol'].values,
+                                                              df_subpoly['lbol_err'].values,df_subpoly['SpT'].values))
+sampler.run_mcmc(pos, nsteps)
+
+# Check what the burn in is so it can be thrown out
+labels = ["slope", "intercept", 'dispersion']
+for k in range(ndim):
+    plt.figure()
+    for n in range(nwalkers):
+        plt.plot(sampler.chain[n,:,k])
+    plt.ylabel(labels[k], fontsize='16')
+    plt.xlabel("step number", fontsize='12')
+
+# Burn in was around 50 steps, so drop 200 to be safe them from the chain
+samples = sampler.chain[:, 200:, :].reshape((-1, ndim))
+
+# Check the corner plot of the chain
+figcheck = corner.corner(samples, labels=["$m$", "$b$",'dispersion'])
 
 # -------------------------------------------------------------------------------------
 # ------------------------- Make Plot: Spt v Teff ------------------------------------
@@ -48,38 +136,19 @@ ax1.annotate('J1256-0224', xy=(12.7, -3.25), color='k', fontsize=12)
 # ---- Add Legend ----
 plt.legend([fld, young, sub], ["Field", "Young", 'Subdwarf'], frameon=False, fontsize=12)
 
-# -------------------------------------------------------------------------------------
-# ------------------------- Polynomial fits  -----------------------------------------
-# -------------------------------------------------------------------------------------
-# ------ Fit polynomial for subdwarfs ------
-# drop nan from column need to get polynomial
-df_subpoly = df_sub[pd.notnull(df_sub['lbol'])]
+# ---- Plot fit lines  from the mcmc, random 100 -----
+xl = np.array([0, 20])
+for x in samples[np.random.randint(len(samples), size=500)]:
+    plt.plot(xl, x[0]*xl+x[1], color="#17becf", alpha=0.05)
 
-# --- Get uncertainites for upper and lower teff limits ----
-df_subpoly['Lbol_up'] = df_subpoly['lbol'] + df_subpoly['lbol_err']
-df_subpoly['Lbol_d'] = df_subpoly['lbol'] - df_subpoly['lbol_err']
+# To get the best fit line
+best_fit_coeffs = np.median(samples, axis=0)
+best_fit_line = best_fit_coeffs[0]*xl + best_fit_coeffs[1]
+plt.plot(xl, best_fit_line, c='k', zorder=8)
 
-# ------ Fit the values --------
-coeffs = np.polyfit(df_subpoly['SpT'], df_subpoly['lbol'], 1)
-line = np.poly1d(coeffs)
-
-coeffs_up = np.polyfit(df_subpoly['SpT'], df_subpoly['Lbol_up'], 1)
-line_up = np.poly1d(coeffs_up)
-
-coeffs_d = np.polyfit(df_subpoly['SpT'], df_subpoly['Lbol_d'], 1)
-line_d = np.poly1d(coeffs_d)
-
-# ---- print values to screen -------
-print coeffs
-print coeffs_up
-print coeffs_d
-
-# ---- Plot the fit lines -----
-xp = np.linspace(5, 30, 100)
-plt.plot(xp, line(xp), '-', color='k')
-plt.plot(xp, line_up(xp), '-', color='#17becf', alpha=.25)
-plt.plot(xp, line_d(xp), '-', color='#17becf', alpha=.25)
-ax1.fill_between(xp, line_up(xp), line_d(xp), alpha=.25, color='#17becf')
+# ---- Print the best fit coeffs and the std -----
+print(best_fit_coeffs)
+print(np.std(samples, axis=0))
 
 plt.tight_layout()
 plt.savefig('Plots/SptvLbol.png')
